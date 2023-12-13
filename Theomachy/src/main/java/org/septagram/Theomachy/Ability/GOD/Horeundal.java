7bce4db6d1775257f15be3dcd1d99412c8fc4209
seunhoo <seunhoo@naver.com>
2023-12-13 11:29:41 +0900

    Update Horeundal.java

--- Theomachy/src/main/java/org/septagram/Theomachy/Ability/GOD/Horeundal.java
+++ Theomachy/src/main/java/org/septagram/Theomachy/Ability/GOD/Horeundal.java
@@ -58,9 +58,9 @@ public class Horeundal extends Ability{
 
 			AtomicInteger count = new AtomicInteger(3);
 			Bukkit.getScheduler().runTaskTimer(Theomachy.getPlugin(), () -> {
+				new HoreunTimer(player, loc, count.getAndDecrement()).run();
 				if(count.get() == 0)
 					return;
-				new HoreunTimer(player, loc, count.getAndDecrement()).run();
 			}, 7 * 20, 1 * 20);
 		}
 		
