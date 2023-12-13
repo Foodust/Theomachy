7aa329a53f86d320ace9b322f6cf16cdfcfc1a9a
seunhoo <seunhoo@naver.com>
2023-12-13 09:35:16 +0900

    123

--- Theomachy/src/main/java/org/septagram/Theomachy/DB/AbilityData.java
+++
@@ -1,8 +0,0 @@
-package org.septagram.Theomachy.DB;
-
-public interface AbilityData
-{
-	int ABILITY_NUMBER=51;
-	int GOD_ABILITY_NUMBER=19;
-	int HUMAN_ABILITY_NUMBER=32;
-}
--- Theomachy/src/main/java/org/septagram/Theomachy/DB/PluginData.java
+++
@@ -1,7 +0,0 @@
-package org.septagram.Theomachy.DB;
-
-public interface PluginData
-{
-	final static String version = "Pi Edition 2.1";
-	final static String buildnumber = "20231212";
-}
